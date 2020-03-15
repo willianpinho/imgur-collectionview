6d1ec0cc43e78e3ec55ac987d73d7adbad9518eb
Willian <willian.pinho@gmail.com>
2020-03-15 17:26:27 -0300

    Initial Commit

---
+++ Imgur CollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Imgur CollectionView
+//
+//  Created by Willian Junior Peres De Pinho on 15/03/20.
+//  Copyright © 2020 Willian Pinho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
